4cea736128d921018ed0988f5d9c10657364f37c
杉山奏 <sugiyamakana@sugiyamakanadenoMacBook-Pro.local>
2026-09-20 13:19:58 +0900

    Create model for reminders table

--- app/Models/Reminders.php
+++ app/Models/Reminders.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Model;
 
 class Reminders extends Model
 {
-    //
+    protected $fillable = [
+        'created_user',
+        'title',
+        'description',
+        'user_received_reminder',
+        'remind_at',
+        'latitude',
+        'longitude',
+        'completed',
+        'repeted',
+    ];
+
+    // Specify property
+    protected $cast =[
+        'remind_at'=>'DateTime',
+        'latitude'=>'decimal:7',
+        'longitude'=>'decimal:7',
+        'completed'=>'boolean',
+        'repeted'=>'boolean',
+    ];
 }
