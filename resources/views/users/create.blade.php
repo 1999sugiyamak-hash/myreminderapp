885b70212f88d88ca348ffaa9676f7b7d7f23df7
杉山奏 <sugiyamakana@sugiyamakanadenoMacBook-Pro.local>
2026-09-25 08:51:17 +0900

    Add the feature to get the webpush user data

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,16 +1,41 @@
 <!-- The view for creating users -->
 
 <head>
+    @extends('layouts.app')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="vapid-public-key" content="{{ config('webpush.vapid.public_key') }}">
+    <script>
+        window.validPublicKey = "{{ config('services.web_push.public_key') }}";
+    </script>
 </head>
-<h1>Create User</h1>
-<!-- TODO: enable resposible design -->
-<form method="POST" action="/users">
-    @csrf
-    <div>
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" required />
-    </div>
 
-</form>
+<body>
+    <h1>Create User</h1>
+    <!-- TODO: enable resposible design -->
+    <form method="POST" action="/users">
+        @csrf
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required />
+        </div>
+    </form>
+    <script>
+        async function registerUser() {
+            const registration = await navigator.serviceWorker.register('/sw.js');
+            await navigator.serviceWorker.ready;
+
+            const permission = await Notification.requestPermission();
+            if (permission !== 'granted') {
+                alert('Please enable notifications');
+                return;
+            }
+
+            const subscription = await registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: window.validPublicKey,
+            })
+            const pushData = subscription.toJSON();
+
+            console.log(pushData);
+        }
+    </script>
+</body>
