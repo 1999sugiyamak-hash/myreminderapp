07ee0ce7ce7dabb36773ab4cbd3cd0023df89eca
杉山奏 <sugiyamakana@sugiyamakanadenoMacBook-Pro.local>
2026-09-25 10:03:23 +0900

    WIP: store user webpush data feature

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Users;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class UsersController extends Controller
 {
     // Return users index
     public function index(){
-
+        // return view('users');
     }
 
     // Return create users page
@@ -19,22 +21,33 @@ class UsersController extends Controller
 
     // Store users information to users DB
     public function store(Request $request){
-        $validated = $request->validate([
+        Log::info($request);
+        try {
+            $validated = $request->validate([
             'name' => 'required|string',
             'endpoint' => 'required|string',
-            'key' => 'required|string',
-            'token' => 'required|string',
-            'encoding' => 'nullable|string'
-        ]);
+            'keys'=>[
 
-       Users::create([
-        'name' => $validated['name'],
-        'endpoint' => $validated['endpoint'],
-        'key' => $validated['key'],
-        'token' => $validated['token'],
-        'encoding' => $validated['encoding'],
-       ]);
+            'auth' => 'required|string',
+            'p256dh' => 'required|string',
+            ],
+            
+            // 'key' => 'required|string',
+            // 'token' => 'required|string',
+            // 'encoding' => 'nullable|string'
+        ]);} catch (Exception $e){
+            $message = $e->getMessage();
+            Log::error($message);
+        }
+        Log::info('finish validate');
+    //    Users::create([
+    //     'name' => $validated['name'],
+    //     'endpoint' => $validated['endpoint'],
+    //     'key' => $validated['keys.p256dh'],
+    //     'token' => $validated['keys.auth'],
+    //     'encoding' => $validated['encoding'],
+    //    ]);
 
-       return redirect('/users');
+    //    return redirect('/users');
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -11,11 +11,14 @@
 <body>
     <h1>Create User</h1>
     <!-- TODO: enable resposible design -->
-    <form method="POST" action="/users">
+    <form method="POST" action="/users" id="create-user-form">
         @csrf
         <div>
             <label for="name">Name</label>
             <input type="text" id="name" name="name" required />
+        </div>
+        <div>
+            <input type="submit" value="Create User!" />
         </div>
     </form>
     <script>
@@ -23,19 +26,60 @@
             const registration = await navigator.serviceWorker.register('/sw.js');
             await navigator.serviceWorker.ready;
 
+            console.log(registration);
             const permission = await Notification.requestPermission();
             if (permission !== 'granted') {
                 alert('Please enable notifications');
                 return;
             }
 
-            const subscription = await registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: window.validPublicKey,
-            })
-            const pushData = subscription.toJSON();
+            // // Get webpush data
+            // const subscription = await registration.pushManager.subscribe({
+            //     userVisibleOnly: true,
+            //     applicationServerKey: window.validPublicKey,
+            // })
+            // const pushData = subscription.toJSON();
 
-            console.log(pushData);
+            // console.log(pushData);
+
+            // Submit webpush data with user name
+            try {
+                const form = document.getElementById('create-user-form')
+                form.addEventListener('submit', async function(e) {
+                    // Get webpush data
+                    e.preventDefault();
+                    const subscription = await registration.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: window.validPublicKey,
+                    })
+                    const pushData = subscription.toJSON();
+
+                    console.log(pushData);
+
+                    await fetch({
+                        method: 'POST',
+                        body: JSON.stringify({
+                            endpoint: pushData.endpoint,
+                            key: pushData.keys.p256ph,
+                            auth: pushData.keys.auth,
+                        }),
+                    });
+                })
+            } catch (e) {
+                console.log(e);
+            }
+            // await fetch(form.action, {
+            //     method: 'POST',
+            //     body: JSON.stringify({
+            //         endpoint: pushData.endpoint,
+            //         key: pushData.keys.p256ph,
+            //         auth: pushData.keys.auth,
+            //     }),
+            // });
+            // console.log(pushData.endpoint);
+            // console.log(pushData.keys);
         }
+
+        registerUser();
     </script>
 </body>
