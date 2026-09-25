1d797a3e8a4499d719493c766e6da0ce433b0df9
杉山奏 <sugiyamakana@sugiyamakanadenoMacBook-Pro.local>
2026-09-25 14:51:10 +0900

    Add timestamp in users database

--- database/migrations/2026_09_22_230511_add_push_information_to_users_table.php
+++ database/migrations/2026_09_22_230511_add_push_information_to_users_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
             $table->string('endpoint');
             $table->string('key');
             $table->string('token');
             $table->string('encoding');
+            $table->timestamps();
         });
     }
 
@@ -24,8 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            Schema::dropIfExists('users');
-        });
+        Schema::dropIfExists('users');
     }
 };
